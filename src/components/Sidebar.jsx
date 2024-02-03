fbac3f82c7741c51ffedc8a0f8282dd01aa5ff56
aka_frnzy <dhakalsuraj950@gmail.com>
2024-02-03 21:10:40 +0545

    Made an active Identifier for The Sidebar Options

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import SearchIcon from '@mui/icons-material/Search';
@@ -11,6 +11,7 @@ import { Link } from 'react-router-dom';
 
 
 const Sidebar = ({ auth }) => {
+    const [meh, setMeh] = useState('Home')
     return (
         <>
             <div className='flex flex-col border-r-2 border-solid pl-5 pr-5'>
@@ -18,12 +19,12 @@ const Sidebar = ({ auth }) => {
                     <img src="https://i.postimg.cc/MHWd0jL6/quack.png" alt="logo" className="h-10 w-14 cursor-pointer mx-4 rounded-full" />
                     <span className='font-bold items-center justify-center'>Quacker</span>
                 </div>
-                <Link to='/'><SidebarTheming text='Home' Icon={HomeIcon} active={true} /></Link>
+                <Link to='/' onClick={() => setMeh("Home")}><SidebarTheming text='Home' Icon={HomeIcon} active={meh == "Home"} /></Link>
                 <SidebarTheming text='Explore' Icon={SearchIcon} />
                 <SidebarTheming text='Notifications' Icon={NotificationsIcon} />
                 <SidebarTheming text='Messages' Icon={MessageIcon} />
                 <SidebarTheming text='Bookmarks' Icon={BookmarkIcon} />
-                <Link to='/profile'><SidebarTheming text='Profile' Icon={AccountCircleIcon} /></Link>
+                <Link to='/profile' onClick={() => setMeh('Profile')}><SidebarTheming text='Profile' Icon={AccountCircleIcon} active={meh == "Profile"} /></Link>
                 <SidebarTheming text='More' Icon={MoreHorizIcon} />
 
                 <button className=
